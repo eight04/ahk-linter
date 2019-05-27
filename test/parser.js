afaad27b92435058a98183569fbb10787e586be6
eight <eight04@gmail.com>
2019-05-27 18:05:05 +0800

    WIP

--- test/parser.js
+++ test/parser.js
@@ -1,15 +1,15 @@
 /* eslint-env mocha */
 const fs = require("fs");
 const assertJSON = require("assert-json");
-const {parseExpression, createState} = require("../lib/parser");
+const {parse} = require("../lib/parser");
 
 describe("cases", () => {
   for (const name of fs.readdirSync(`${__dirname}/cases`)) {
     it(name, () => {
       const code = fs.readFileSync(`${__dirname}/cases/${name}/input.ahk`, "utf8").replace(/\r\n|\r/g, "\n");
-      const state = createState(code);
-      parseExpression(state);
-      assertJSON.equalFile(state.value, `${__dirname}/cases/${name}/output.json`);
+      const ast = parse(code);
+      const actual = ast.body.length <= 1 ? ast.body[0] : ast.body;
+      assertJSON.equalFile(actual, `${__dirname}/cases/${name}/output.json`);
     });
   }
 });
